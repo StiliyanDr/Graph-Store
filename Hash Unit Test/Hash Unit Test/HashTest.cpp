717d6091ed475dcaf5e56ced95c88ffb6f54544e
StiliyanDr <stiliyan.dragnev@gmail.com>
2018-03-17 00:36:31 +0200

    Added unit tests for the special members of the Hash class.

--- Hash Unit Test/Hash Unit Test/HashTest.cpp
+++ Hash Unit Test/Hash Unit Test/HashTest.cpp
@@ -21,7 +21,7 @@ namespace HashUnitTest
 		void fillHash(Hash& hash, size_t from, size_t to)
 		{
 			while (from <= to)
-				hash.insert(books[from++]);
+				hash.add(books[from++]);
 		}
 
 		TEST_CLASS_INITIALIZE(HashTestInitialise)
@@ -56,7 +56,122 @@ namespace HashUnitTest
 			}
 		}
 
-		TEST_METHOD(insertingAndSearching)
+		TEST_METHOD(moveConstruction)
+		{
+			Hash hash(1);
+			
+			Hash fromEmpty(std::move(hash));
+			Assert::IsTrue(fromEmpty.isEmpty() && hash.isEmpty());
+
+			fillHash(hash, 0, ARRAY_SIZE - 1);
+			
+			Hash fromNonEmpty(std::move(hash));
+			Assert::IsTrue(hash.isEmpty());
+
+			fillHash(hash, 0, ARRAY_SIZE - 1);
+
+			for (int i = 0; i < ARRAY_SIZE - 1; ++i)
+			{
+				Assert::IsNotNull(hash.search(books[i].getTitle()));
+				Assert::IsNotNull(fromNonEmpty.search(books[i].getTitle()));
+			}
+		}
+
+		TEST_METHOD(copyConstruction)
+		{
+			Hash hash(10);
+
+			Hash fromEmpty(hash);
+			Assert::IsTrue(fromEmpty.isEmpty());
+		
+			fillHash(hash, 0, ARRAY_SIZE - 1);
+
+			Hash fromNonEmpty(hash);
+			Assert::AreEqual(hash.getCount(), fromNonEmpty.getCount());
+
+			for (int i = 0; i < ARRAY_SIZE - 1; ++i)
+				Assert::IsNotNull(fromNonEmpty.search(books[i].getTitle()));
+		}
+
+		TEST_METHOD(moveAssigningEmptyObject)
+		{
+			Hash empty(1);
+
+			Hash lhs(1);
+			lhs = std::move(empty);
+			Assert::IsTrue(lhs.isEmpty() && empty.isEmpty());
+
+			fillHash(lhs, 0, ARRAY_SIZE - 1);
+
+			lhs = std::move(empty);
+			Assert::IsTrue(lhs.isEmpty() && empty.isEmpty());
+		}
+
+		TEST_METHOD(moveAssigningNonEmptyObject)
+		{
+			size_t middle = ARRAY_SIZE / 2;
+
+			Hash rhs(ARRAY_SIZE), lhs(1);
+			fillHash(rhs, 0, middle - 1);
+
+			lhs = std::move(rhs);
+			Assert::IsTrue(rhs.isEmpty());
+
+			for (size_t i = 0; i < middle; ++i)
+				Assert::IsNotNull(lhs.search(books[i].getTitle()));
+
+			fillHash(rhs, middle, ARRAY_SIZE - 1);
+
+			lhs = std::move(rhs);
+			Assert::IsTrue(rhs.isEmpty());
+
+			for (size_t i = 0; i < middle; ++i)
+				Assert::IsNull(lhs.search(books[i].getTitle()));
+
+			for (size_t i = middle; i < ARRAY_SIZE; ++i)
+				Assert::IsNotNull(lhs.search(books[i].getTitle()));
+		}
+
+		TEST_METHOD(copyAssigningEmptyObject)
+		{
+			Hash empty(1), lhs(ARRAY_SIZE);
+
+			fillHash(lhs, 0, ARRAY_SIZE - 1);
+
+			for (int i = 1; i <= 2; ++i)
+			{
+				lhs = empty;
+				Assert::IsTrue(empty.isEmpty() && lhs.isEmpty());
+			}
+		}
+
+		TEST_METHOD(copyAssigningNonEmptyObject)
+		{
+			size_t middle = ARRAY_SIZE / 2;
+
+			Hash lhs(1), rhs(ARRAY_SIZE);
+
+			fillHash(rhs, 0, middle - 1);
+
+			lhs = rhs;
+			Assert::AreEqual(middle, rhs.getCount());
+
+			for (size_t i = 0; i < middle; ++i)
+				Assert::IsNotNull(lhs.search(books[i].getTitle()));
+
+			fillHash(rhs, middle, ARRAY_SIZE - 1);
+
+			lhs = rhs;
+			Assert::AreEqual(ARRAY_SIZE, rhs.getCount());
+
+			for (size_t i = 0; i < ARRAY_SIZE; ++i)
+			{
+				Assert::IsNotNull(lhs.search(books[i].getTitle()));
+				Assert::IsNotNull(rhs.search(books[i].getTitle()));
+			}
+		}
+
+		TEST_METHOD(addingAndSearching)
 		{
 			Hash hash(ARRAY_SIZE);
 
@@ -84,7 +199,7 @@ namespace HashUnitTest
 
 			for (size_t i = 0; i < ARRAY_SIZE; ++i)
 			{
-				hash.insert(books[i]);
+				hash.add(books[i]);
 				Assert::AreEqual(i + 1, hash.getCount());
 			}
 		}
