4775a0d6272023779f07e3d14fe5dc1a8aa7dc87
StiliyanDr <stiliyan.dragnev@gmail.com>
2018-05-06 23:04:31 +0300

    Add typedefs for iterators in Graph's interface
    The methods that return iterators actually return smart references to
    iterators and the resulting type is complex. A typedef will make their
    use much easier.

--- Graph Store/Graph Store/Graph.h
+++ Graph Store/Graph Store/Graph.h
@@ -5,9 +5,14 @@
 #include "Iterator.h"
 #include "Vertex.h"
 #include "Edge.h"
+#include <memory>
 
 class Graph
 {
+protected:
+	typedef std::unique_ptr<Iterator<Vertex*>> VertexAbstractIterator;
+	typedef std::unique_ptr<Iterator<Edge>> EdgeAbstractIterator;
+
 public:
 	Graph(const Graph&) = delete;
 	Graph& operator=(const Graph&) = delete;
@@ -18,8 +23,8 @@ public:
 	virtual void addEdgeFromToWithWeight(const char* startVertex, const char* endVertex, unsigned weight) = 0;
 	virtual void removeEdgeFromTo(const char* startVertex, const char* endVertex) = 0;
 
-	virtual Iterator<Vertex*> getIteratorOfVertices() = 0;
-	virtual Iterator<Edge> getIteratorOfEdgesStartingFrom(Vertex& vertex) = 0;
+	virtual VertexAbstractIterator getIteratorOfVertices() = 0;
+	virtual EdgeAbstractIterator getIteratorOfEdgesStartingFrom(Vertex& vertex) = 0;
 
 	const String& getIdentifier() const;
 
