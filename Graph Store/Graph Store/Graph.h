6f36f73c6001e3b1743ce599b472db7313db559f
StiliyanDr <stiliyan.dragnev@gmail.com>
2018-05-12 23:35:35 +0300

    Change Graph's interface
    Add a public getVertexWithIdentifier method to the interface.
    
    Most of the methods in the current version take vertex identifiers as
    arguments and have to search for the vertices before doing anything
    else.
    
    The methods mentioned above can now take the vertices themselves as
    arguments, which is more efficient since the client code already has
    access to them and there is no need of an extra search.

--- Graph Store/Graph Store/Graph.h
+++ Graph Store/Graph Store/Graph.h
@@ -17,12 +17,13 @@ public:
 	Graph(const Graph&) = delete;
 	Graph& operator=(const Graph&) = delete;
 	virtual ~Graph() = default;
-	
+		
 	virtual void addVertex(const char* identifier) = 0;
-	virtual void removeVertex(const char* identifier) = 0;
-	virtual void addEdgeFromToWithWeight(const char* startVertex, const char* endVertex, unsigned weight) = 0;
-	virtual void removeEdgeFromTo(const char* startVertex, const char* endVertex) = 0;
+	virtual void removeVertex(Vertex& vertexToRemove) = 0;
+	virtual void addEdgeBetweenWithWeight(Vertex& startVertex, Vertex& endVertex, unsigned weight) = 0;
+	virtual void removeEdgeBetween(Vertex& startVertex, const Vertex& endVertex) = 0;
 
+	virtual Vertex* getVertexWithIdentifier(const char* identifier) = 0;
 	virtual VertexAbstractIterator getIteratorOfVertices() = 0;
 	virtual EdgeAbstractIterator getIteratorOfEdgesStartingFrom(Vertex& vertex) = 0;
 
