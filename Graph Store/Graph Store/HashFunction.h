94141befa235476cc7d37a06f6ea3dc06779f98a
StiliyanDr <stiliyan.dragnev@gmail.com>
2018-06-10 17:22:28 +0300

    Remove an unnecessary comment from HashFunction.h

--- Graph Store/Graph Store/HashFunction.h
+++ Graph Store/Graph Store/HashFunction.h
@@ -1,10 +1,3 @@
-/*
-	The hash function used in the Hash data structure.
-	This class implements an identity function: the argument 
-	is casted to size_t and the result is returned.
-	Template class specialization should be used for different behaviour.
-*/
-
 #ifndef __HASH_FUNCTION_HEADER_INCLUDED__
 #define __HASH_FUNCTION_HEADER_INCLUDED__
 
@@ -12,9 +5,9 @@ template <class Key>
 class HashFunction
 {
 public:
-	size_t operator()(const Key& key) const
+	unsigned operator()(const Key& key) const
 	{
-		return static_cast<size_t>(key);
+		return static_cast<unsigned>(key);
 	}
 };
 
