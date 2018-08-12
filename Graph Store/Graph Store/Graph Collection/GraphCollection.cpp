64d61652cae01a3771ba595cfbfd55b4fede6a6a
StiliyanDr <stiliyan.dragnev@gmail.com>
2018-08-12 18:26:31 +0300

    Add the GraphCollection class

---
+++ Graph Store/Graph Store/Graph Collection/GraphCollection.cpp
@@ -0,0 +1,124 @@
+#include "GraphCollection.h"
+
+GraphCollection& GraphCollection::operator=(GraphCollection&& rhs)
+{
+	if (this != &rhs)
+	{
+		empty();
+		graphs = std::move(rhs.graphs);
+	}
+
+	return *this;
+}
+
+void GraphCollection::empty()
+{
+	destroyAllGraphs();
+	graphs.empty();
+}
+
+void GraphCollection::destroyAllGraphs()
+{
+	CollectionIterator iterator = graphs.getIteratorToFirst();
+
+	while (iterator.isValid())
+	{
+		delete iterator.getCurrentItem();
+
+		iterator.advance();
+	}
+}
+
+GraphCollection::~GraphCollection()
+{
+	destroyAllGraphs();
+}
+
+void GraphCollection::add(std::unique_ptr<Graph> graph)
+{
+	const String& id = graph->getIdentifier();
+
+	if (!hasGraphWithID(id))
+	{
+		tryToAdd(std::move(graph));
+	}
+	else
+	{
+		throw std::runtime_error("There already is a graph with id: " + id);
+	}
+}
+
+bool GraphCollection::hasGraphWithID(const String& id) const
+{
+	size_t count = graphs.getCount();
+
+	for (size_t i = 0; i < count; ++i)
+	{
+		if (graphs[i]->getIdentifier() == id)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void GraphCollection::tryToAdd(std::unique_ptr<Graph> graph)
+{
+	try
+	{
+		graphs.add(graph.get());
+	}
+	catch (std::bad_alloc&)
+	{
+		throw std::runtime_error("Not enough memory to add a graph!");
+	}
+
+	graph.release();
+}
+
+void GraphCollection::remove(const String& graphID)
+{
+	size_t index = getIndexOfGraphWithID(graphID);
+	delete graphs[index];
+	graphs.removeAt(index);
+}
+
+size_t GraphCollection::getIndexOfGraphWithID(const String& id) const
+{
+	size_t count = graphs.getCount();
+
+	for (size_t i = 0; i < count; ++i)
+	{
+		if (graphs[i]->getIdentifier() == id)
+		{
+			return i;
+		}
+	}
+
+	throw std::runtime_error("There is no graph with id: " + id);
+}
+
+Graph& GraphCollection::getGraphWithID(const String& id)
+{
+	size_t index = getIndexOfGraphWithID(id);
+	
+	return *graphs[index];
+}
+
+std::unique_ptr<Iterator<Graph*>> GraphCollection::getIterator()
+{
+	CollectionIterator iterator = graphs.getIteratorToFirst();
+
+	return std::unique_ptr<Iterator<Graph*>>(new CollectionIterator(iterator));
+}
+
+bool GraphCollection::isEmpty() const
+{
+	return getCount() == 0;
+}
+
+size_t GraphCollection::getCount() const
+{
+	return graphs.getCount();
+}
---
+++ Graph Store/Graph Store/Graph Collection/GraphCollection.h
@@ -0,0 +1,40 @@
+#ifndef __GRAPH_COLLECTION_HEADER_INCLUDED__
+#define __GRAPH_COLLECTION_HEADER_INCLUDED__
+
+#include "../Dynamic Array/DynamicArray.h"
+#include "../Graph/Abstract class/Graph.h"
+#include "../Iterator/Iterator.h"
+
+class GraphCollection
+{
+	typedef DynamicArray<Graph*> Collection;
+	typedef DynamicArrayIterator<Graph*> CollectionIterator;
+
+public:
+	GraphCollection() = default;
+	GraphCollection(const GraphCollection&) = delete;
+	GraphCollection& operator=(const GraphCollection&) = delete;
+	GraphCollection(GraphCollection&&) = default;
+	GraphCollection& operator=(GraphCollection&& rhs);
+	~GraphCollection();
+
+	void add(std::unique_ptr<Graph> graph);
+	void remove(const String& graphID);
+	Graph& getGraphWithID(const String& id);
+	
+	std::unique_ptr<Iterator<Graph*>> getIterator();
+	size_t getCount() const;
+	bool isEmpty() const;
+	void empty();
+
+private:
+	bool hasGraphWithID(const String& id) const;
+	void tryToAdd(std::unique_ptr<Graph> graph);
+	size_t getIndexOfGraphWithID(const String& id) const;
+	void destroyAllGraphs();
+
+private:
+	Collection graphs;
+};
+
+#endif //__GRAPH_COLLECTION_HEADER_INCLUDED__
