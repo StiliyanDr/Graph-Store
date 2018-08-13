d969c70e300836adbc066314bd358edd6e2c09e9
StiliyanDr <stiliyan.dragnev@gmail.com>
2018-08-13 22:26:43 +0300

    Add the UseGraphCommand class

---
+++ Graph Store/Graph Store/Command/Use Graph Command/UseGraphCommand.cpp
@@ -0,0 +1,36 @@
+#include "UseGraphCommand.h"
+#include "../Command Registrator/CommandRegistrator.h"
+
+static CommandRegistrator<UseGraphCommand> registrator("USE-GRAPH", "Sets a specified graph as the used one");
+
+void UseGraphCommand::execute(args::Subparser& parser)
+{
+	parseArguments(parser);
+	useGraph(graphID);
+}
+
+void UseGraphCommand::parseArguments(args::Subparser& parser)
+{
+	args::Positional<String, StringReader> graphID(parser, "graph id", "The identifier of the graph to use");
+	parser.Parse();
+	setGraphID(graphID);
+}
+
+void UseGraphCommand::setGraphID(args::Positional<String, StringReader>& graphID)
+{
+	if (graphID.Matched())
+	{
+		this->graphID = args::get(graphID);
+	}
+	else
+	{
+		throw std::runtime_error("Missing argument: [graph id]!");
+	}
+}
+
+void UseGraphCommand::useGraph(const String& id)
+{
+	GraphCollection& graphs = getGraphs();
+	Graph& graph = graphs.getGraphWithID(id);
+	setUsedGraph(graph);
+}
---
+++ Graph Store/Graph Store/Command/Use Graph Command/UseGraphCommand.h
@@ -0,0 +1,30 @@
+#ifndef __USE_GRAPH_COMMAND_HEADER_INCLUDED__
+#define __USE_GRAPH_COMMAND_HEADER_INCLUDED__
+
+#include "../Abstract class/Command.h"
+#include "../String Reader/StringReader.h"
+
+class UseGraphCommand : public Command
+{
+public:
+	UseGraphCommand() = default;
+	UseGraphCommand(const UseGraphCommand&) = delete;
+	UseGraphCommand& operator=(const UseGraphCommand&) = delete;
+	UseGraphCommand(UseGraphCommand&&) = delete;
+	UseGraphCommand& operator=(UseGraphCommand&&) = delete;
+	virtual ~UseGraphCommand() = default;
+
+	virtual void execute(args::Subparser& parser) override;
+
+private:
+	static void useGraph(const String& id);
+
+private:
+	void parseArguments(args::Subparser& parser);
+	void setGraphID(args::Positional<String, StringReader>& graphID);
+
+private:
+	String graphID;
+};
+
+#endif //__USE_GRAPH_COMMAND_HEADER_INCLUDED__
