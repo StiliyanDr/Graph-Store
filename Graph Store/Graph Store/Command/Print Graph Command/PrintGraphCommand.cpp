4d0566504e3fbb3761a00e02579798c5e7348361
StiliyanDr <stiliyan.dragnev@gmail.com>
2018-08-13 19:12:04 +0300

    Add the PrintGraphCommand class

---
+++ Graph Store/Graph Store/Command/Print Graph Command/PrintGraphCommand.cpp
@@ -0,0 +1,73 @@
+#include "PrintGraphCommand.h"
+#include "../Command Registrator/CommandRegistrator.h"
+#include <iostream>
+
+static CommandRegistrator<PrintGraphCommand> registrator("PRINT-GRAPH", "Prints a specified graph");
+
+void PrintGraphCommand::execute(args::Subparser& parser)
+{
+	parseArguments(parser);
+	printGraph(graphID);
+}
+
+void PrintGraphCommand::parseArguments(args::Subparser& parser)
+{
+	args::Positional<String, StringReader> graphID(parser, "graph id", "The identifier of the graph to print");
+	parser.Parse();
+	setGraphID(graphID);
+}
+
+void PrintGraphCommand::setGraphID(args::Positional<String, StringReader>& graphID)
+{
+	if (graphID.Matched())
+	{
+		this->graphID = args::get(graphID);
+	}
+	else
+	{
+		throw std::runtime_error("Missing argument: [graph id]!");
+	}
+}
+
+void PrintGraphCommand::printGraph(const String& graphID)
+{
+	GraphCollection& graphs = getGraphs();
+	Graph& graph = graphs.getGraphWithID(graphID);
+
+	std::unique_ptr<Iterator<Vertex*>> iterator = graph.getIteratorOfVertices();
+	Vertex* vertex;
+
+	while (iterator->isValid())
+	{
+		vertex = iterator->getCurrentItem();
+
+		printEdgesLeaving(*vertex, graph);
+
+		iterator->advance();
+	}
+}
+
+void PrintGraphCommand::printEdgesLeaving(Vertex& vertex, Graph& graph)
+{
+	std::cout << vertex.getIdentifier() << ": ";
+
+	std::unique_ptr<Iterator<Edge>> iterator =
+		graph.getIteratorOfEdgesStartingFrom(vertex);
+
+	while (iterator->isValid())
+	{
+		printEdge(iterator->getCurrentItem());
+
+		iterator->advance();
+	}
+
+	std::cout << '\n';
+}
+
+void PrintGraphCommand::printEdge(Edge& edge)
+{
+	Vertex& endOfEdge = edge.getVertex();
+
+	std::cout << "(" << edge.getWeight() << ", "
+			  << endOfEdge.getIdentifier() << "), ";
+}
---
+++ Graph Store/Graph Store/Command/Print Graph Command/PrintGraphCommand.h
@@ -0,0 +1,32 @@
+#ifndef __PRINT_GRAPH_COMMAND_HEADER_INCLUDED__
+#define __PRINT_GRAPH_COMMAND_HEADER_INCLUDED__
+
+#include "../Abstract class/Command.h"
+#include "../String Reader/StringReader.h"
+
+class PrintGraphCommand : public Command
+{
+public:
+	PrintGraphCommand() = default;
+	PrintGraphCommand(const PrintGraphCommand&) = default;
+	PrintGraphCommand& operator=(const PrintGraphCommand&) = default;
+	PrintGraphCommand(PrintGraphCommand&&) = default;
+	PrintGraphCommand& operator=(PrintGraphCommand&&) = default;
+	virtual ~PrintGraphCommand() = default;
+
+	virtual void execute(args::Subparser& parser) override;
+
+private:
+	static void printGraph(const String& graphID);
+	static void printEdgesLeaving(Vertex& vertex, Graph& graph);
+	static void printEdge(Edge& edge);
+
+private:
+	void parseArguments(args::Subparser& parser);
+	void setGraphID(args::Positional<String, StringReader>& graphID);
+
+private:
+	String graphID;
+};
+
+#endif //__PRINT_GRAPH_COMMAND_HEADER_INCLUDED__
