69758ccc3f561c0b0b7548b3eb0a71ad10b170fa
StiliyanDr <stiliyan.dragnev@gmail.com>
2018-08-14 23:08:31 +0300

    Add the SearchCommandClass

---
+++ Graph Store/Graph Store/Command/Search Command/SearchCommand.cpp
@@ -0,0 +1,77 @@
+#include "SearchCommand.h"
+#include "../../Shortest Path Algorithms/Store/ShortestPathAlgorithmsStore.h"
+#include "../Command Registrator/CommandRegistrator.h"
+
+static CommandRegistrator<SearchCommand> registrator("SEARCH", "Finds a shortest path between two vertices");
+
+void SearchCommand::execute(args::Subparser& parser)
+{
+	parseArguments(parser);
+	PathEnds ends = findShortestPath(sourceID, targetID, algorithmID);
+	printPath(ends.source, ends.target);
+}
+
+void SearchCommand::parseArguments(args::Subparser& parser)
+{
+	args::Positional<String, StringReader> sourceID(parser, "source id", "The identifier of the source vertex");
+	args::Positional<String, StringReader> targetID(parser, "target id", "The identifier of the target vertex");
+	args::Positional<String, StringReader> algorithmID(parser, "algorithm id", "The algorithm to use");
+
+	parser.Parse();
+
+	setSourceID(sourceID);
+	setTargetID(targetID);
+	setAlgorithmID(algorithmID);
+}
+
+void SearchCommand::setSourceID(args::Positional<String, StringReader>& id)
+{
+	if (id.Matched())
+	{
+		sourceID = args::get(id);
+	}
+	else
+	{
+		throw std::runtime_error("Missing argument: [" + id.Name() + "]!");
+	}
+}
+
+void SearchCommand::setTargetID(args::Positional<String, StringReader>& id)
+{
+	if (id.Matched())
+	{
+		targetID = args::get(id);
+	}
+	else
+	{
+		throw std::runtime_error("Missing argument: [" + id.Name() + "]!");
+	}
+}
+
+void SearchCommand::setAlgorithmID(args::Positional<String, StringReader>& id)
+{
+	if (id.Matched())
+	{
+		algorithmID = args::get(id);
+	}
+	else
+	{
+		throw std::runtime_error("Missing argument: [" + id.Name() + "]!");
+	}
+}
+
+SearchCommand::PathEnds SearchCommand::findShortestPath(const String& sourceID,
+														const String& targetID,
+														const String& algorithmID)
+{
+	Graph& usedGraph = getUsedGraph();
+	Vertex& source = usedGraph.getVertexWithIdentifier(sourceID);
+	Vertex& target = usedGraph.getVertexWithIdentifier(targetID);
+
+	ShortestPathAlgorithm& algorithm =
+		ShortestPathAlgorithmsStore::instance().searchForAlgorithm(algorithmID);
+
+	algorithm.findShortestPath(usedGraph, source, target);
+
+	return PathEnds(source, target);
+}
---
+++ Graph Store/Graph Store/Command/Search Command/SearchCommand.h
@@ -0,0 +1,45 @@
+#ifndef __SEARCH_COMMAND_HEADER_INCLUDED__
+#define __SEARCH_COMMAND_HEADER_INCLUDED__
+
+#include "../Abstract class/Command.h"
+#include "../String Reader/StringReader.h"
+
+class SearchCommand : public Command
+{
+	struct PathEnds
+	{
+		PathEnds(Vertex& source, Vertex& target) :
+			source(source), target(target) { }
+
+		Vertex& source;
+		Vertex& target;
+	};
+
+public:
+	SearchCommand() = default;
+	SearchCommand(const SearchCommand&) = delete;
+	SearchCommand& operator=(const SearchCommand&) = delete;
+	SearchCommand(SearchCommand&&) = delete;
+	SearchCommand& operator=(SearchCommand&&) = delete;
+	virtual ~SearchCommand() = default;
+	
+	virtual void execute(args::Subparser& parser) override;
+
+private:
+	static PathEnds findShortestPath(const String& sourceID,
+									 const String& targetID,
+									 const String& algorithmID);
+
+private:
+	void parseArguments(args::Subparser& parser);
+	void setSourceID(args::Positional<String, StringReader>& id);
+	void setTargetID(args::Positional<String, StringReader>& id);
+	void setAlgorithmID(args::Positional<String, StringReader>& id);
+
+private:
+	String sourceID;
+	String targetID;
+	String algorithmID;
+};
+
+#endif //__SEARCH_COMMAND_HEADER_INCLUDED__
