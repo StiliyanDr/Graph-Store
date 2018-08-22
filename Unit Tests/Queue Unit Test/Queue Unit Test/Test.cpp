da6df546882470b73a9c88282fe9b2ad1f7282a2
StiliyanDr <stiliyan.dragnev@gmail.com>
2018-08-22 23:31:27 +0300

    Add a couple of unit tests for the Queue class

--- Unit Tests/Queue Unit Test/Queue Unit Test/Test.cpp
+++ Unit Tests/Queue Unit Test/Queue Unit Test/Test.cpp
@@ -10,8 +10,13 @@ namespace QueueUnitTest
 	{
 		typedef Queue<int> Queue;
 
-	public:
-		Queue createQueueFromRange(int firstNumber, int lastNumber)
+	private:
+		static bool areEqual(const char* lhs, const char* rhs)
+		{
+			return strcmp(lhs, rhs) == 0;
+		}
+
+		static Queue createQueueFromRange(int firstNumber, int lastNumber)
 		{
 			Queue queue;
 
@@ -23,6 +28,7 @@ namespace QueueUnitTest
 			return queue;
 		}
 
+	public:
 		TEST_METHOD(testDefaultConstructorCreatesAnEmptyQueue)
 		{
 			Queue queue;
@@ -47,11 +53,41 @@ namespace QueueUnitTest
 			Assert::IsTrue(queue.isEmpty());
 		}
 
+		TEST_METHOD(testDequeueOnEmptyQueueThrowsException)
+		{
+			Queue queue;
+
+			try
+			{
+				queue.dequeue();
+				Assert::Fail(L"The method did not throw an exception!");
+			}
+			catch (std::logic_error& e)
+			{
+				Assert::IsTrue(areEqual("The queue is empty!", e.what()));
+			}
+		}
+
 		TEST_METHOD(testFirstReturnsTheHeadOfTheQueue)
 		{
 			Queue queue = createQueueFromRange(1, 5);
 
 			Assert::AreEqual(1, queue.first());
+		}
+
+		TEST_METHOD(testFirstOnEmptyQueueThrowsException)
+		{
+			Queue queue;
+
+			try
+			{
+				queue.first();
+				Assert::Fail(L"The method did not throw an exception!");
+			}
+			catch (std::logic_error& e)
+			{
+				Assert::IsTrue(areEqual("The queue is empty!", e.what()));
+			}
 		}
 	};
 }
