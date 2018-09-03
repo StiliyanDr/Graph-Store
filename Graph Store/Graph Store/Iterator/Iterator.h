dd9b5434c5dd9aa1ea5b685ed90ea1416edadab8
StiliyanDr <stiliyan.dragnev@gmail.com>
2018-09-03 22:11:39 +0300

    Modify the Iterator interface
    The interface will now support const and non-const iterators.

--- Graph Store/Graph Store/Iterator/Iterator.h
+++ Graph Store/Graph Store/Iterator/Iterator.h
@@ -1,15 +1,21 @@
 #ifndef __ITERATOR_HEADER_INCLUDED__
 #define __ITERATOR_HEADER_INCLUDED__
 
-template <class Item>
+#include <type_traits>
+
+template <class Item, bool isConst = false>
 class Iterator
 {
+protected:
+	typedef typename std::conditional<isConst, const Item&, Item&>::type Reference;
+	typedef typename std::conditional<isConst, const Item*, Item*>::type Pointer;
+
 public:
 	virtual ~Iterator() = default;
 
 	virtual Iterator<Item>& operator++() = 0;
-	virtual Item* operator->() = 0;
-	virtual Item& operator*() = 0;
+	virtual Reference operator*() const = 0;
+	virtual Pointer operator->() const = 0;
 	virtual bool operator!() const = 0;
 	virtual operator bool() const = 0;
 
