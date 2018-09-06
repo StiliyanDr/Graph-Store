5f343a45271dedb24019a762110097819658447a
StiliyanDr <stiliyan.dragnev@gmail.com>
2018-09-06 22:26:04 +0300

    Remove attributes for algorithms from Vertex

--- Graph Store/Graph Store/Graph/Vertex/Vertex.cpp
+++ Graph Store/Graph Store/Graph/Vertex/Vertex.cpp
@@ -5,64 +5,6 @@ Vertex::Vertex(String id, size_t index) :
 	index(index)
 {
 	setID(std::move(id));
-	setParent(nullptr);
-	setDistance(Distance::getInfinity());
-	markAsNotVisited();
-}
-
-bool Vertex::isMarkedAsVisited() const
-{
-	return isVisited;
-}
-
-void Vertex::markAsVisited()
-{
-	isVisited = true;
-}
-
-void Vertex::markAsNotVisited()
-{
-	isVisited = false;
-}
-
-void Vertex::setParent(Vertex* parent)
-{
-	this->parent = parent;
-}
-
-Vertex* Vertex::getParent()
-{
-	return parent;
-}
-
-const Vertex* Vertex::getParent() const
-{
-	return parent;
-}
-
-void Vertex::setDistance(const Distance& distance)
-{
-	this->distance = distance;
-}
-
-const Distance& Vertex::getDistance() const
-{
-	return distance;
-}
-
-void Vertex::setPriorityQueueHandle(const PriorityQueueHandle& handle)
-{
-	this->handle = handle;
-}
-
-const PriorityQueueHandle& Vertex::getPriorityQueueHandle() const
-{
-	return handle;
-}
-
-const String& Vertex::getID() const
-{
-	return id;
 }
 
 void Vertex::setID(String id)
@@ -73,8 +15,13 @@ void Vertex::setID(String id)
 	}
 	else
 	{
-		throw GraphException("Invalid vertex identifier: " + id);
+		throw GraphException("Invalid vertex id: " + id);
 	}
+}
+
+const String& Vertex::getID() const
+{
+	return id;
 }
 
 bool operator!=(const Vertex& lhs, const Vertex& rhs)
--- Graph Store/Graph Store/Graph/Vertex/Vertex.h
+++ Graph Store/Graph Store/Graph/Vertex/Vertex.h
@@ -3,9 +3,7 @@
 
 #include "../../String/String.h"
 #include "../../Linked List/LinkedList.h"
-#include "../../Distance/Distance.h"
 #include "../Edge/Edge.h"
-#include "../../Priority Queue/Handle/PriorityQueueHandle.h"
 
 class Vertex
 {
@@ -14,39 +12,17 @@ class Vertex
 public:
 	Vertex(const Vertex&) = delete;
 	Vertex& operator=(const Vertex&) = delete;
-	Vertex(Vertex&&) = delete;
-	Vertex& operator=(Vertex&&) = delete;
-	~Vertex() = default;
-
-	bool isMarkedAsVisited() const;
-	void markAsVisited();
-	void markAsNotVisited();
-
-	void setParent(Vertex* parent);
-	Vertex* getParent();
-	const Vertex* getParent() const;
-
-	void setDistance(const Distance& distance);
-	const Distance& getDistance() const;
-
-	void setPriorityQueueHandle(const PriorityQueueHandle& handle);
-	const PriorityQueueHandle& getPriorityQueueHandle() const;
 
 	const String& getID() const;
+	void setID(String id);
 
 private:
 	Vertex(String id, size_t index);
-	
-	void setID(String id);
 
 private:
 	String id;
 	size_t index;
 	LinkedList<Edge> edges;
-	Vertex* parent;
-	PriorityQueueHandle handle;
-	Distance distance;
-	bool isVisited;
 };
 
 bool operator==(const Vertex& lhs, const Vertex& rhs);
