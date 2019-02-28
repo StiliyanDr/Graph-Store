0e75bbe3c42d2e4bae8aede18b4a9fffe6f370f2
StiliyanDr <stiliyan.dragnev@gmail.com>
2019-02-28 20:57:05 +0200

    Add the DirectoryIterator class

---
+++ Graph Store/Graph Store/Directory Files Iterator/DirectoryIterator.cpp
@@ -0,0 +1,103 @@
+#include "DirectoryIterator.h"
+#include "Directory Files Iterator Exception\DirectoryIteratorException.h"
+#include "String\String.h"
+#include <assert.h>
+#include <stdexcept>
+
+const DirectoryIterator::Iterator DirectoryIterator::end;
+
+DirectoryIterator::DirectoryIterator(const String& path,
+	                                 ExtensionFilter f) :
+	filter(std::move(f))
+{
+	tryToStartIterationIn(path);
+	skipNonpassingFiles();
+}
+
+void DirectoryIterator::tryToStartIterationIn(const String& path)
+{
+	assert(!isValid());
+
+	try
+	{
+		iterator = Iterator(Path(path.cString()));
+	}
+	catch (std::experimental::filesystem::filesystem_error& e)
+	{
+		throw DirectoryIteratorException(String(e.what()));
+	}
+}
+
+void DirectoryIterator::skipNonpassingFiles()
+{
+	while (isValid() && !filter(currentExtension()))
+	{
+		++iterator;
+	}
+}
+
+String DirectoryIterator::currentExtension() const
+{
+	assert(isValid());
+
+	const Path& p = iterator->path();
+	
+	return pathToString(p.extension());
+}
+
+String DirectoryIterator::pathToString(const Path& p)
+{
+	std::string s = p.string();
+
+	return String(s.c_str());
+}
+
+String DirectoryIterator::operator*() const
+{
+	if (isValid())
+	{
+		return pathToString(iterator->path());
+	}
+	else
+	{
+		throw std::out_of_range("Iterator out of range!");
+	}
+}
+
+DirectoryIterator& DirectoryIterator::operator++()
+{
+	if (isValid())
+	{
+		++iterator;
+		skipNonpassingFiles();
+	}
+
+	return *this;
+}
+
+DirectoryIterator::operator bool() const
+{
+	return isValid();
+}
+
+bool DirectoryIterator::operator!() const
+{
+	return !isValid();
+}
+
+bool DirectoryIterator::isValid() const
+{
+	return iterator != end;
+}
+
+bool operator!=(const DirectoryIterator& lhs,
+	            const DirectoryIterator& rhs)
+{
+	return !(lhs == rhs);
+}
+
+bool operator==(const DirectoryIterator& lhs,
+	            const DirectoryIterator& rhs)
+{
+	return lhs.iterator == rhs.iterator;
+}
---
+++ Graph Store/Graph Store/Directory Files Iterator/DirectoryIterator.h
@@ -0,0 +1,59 @@
+#ifndef __DIRECTORY_ITERATOR_HEADER_INCLUDED__
+#define __DIRECTORY_ITERATOR_HEADER_INCLUDED__
+
+#include <experimental\filesystem>
+#include <functional>
+
+class String;
+
+class DirectoryIterator
+{
+	using Iterator = std::experimental::filesystem::directory_iterator;
+	using Path = std::experimental::filesystem::path;
+
+public:
+	using ExtensionFilter = std::function<bool (const String&)>;
+
+	class AlwaysTrue
+	{
+	public:
+		bool operator()(const String&) const noexcept
+		{
+			return true;
+		}
+	};
+
+public:
+	DirectoryIterator() = default;
+	explicit DirectoryIterator(const String& path,
+		                       ExtensionFilter filter = AlwaysTrue());
+	~DirectoryIterator() = default;
+
+	DirectoryIterator& operator++();
+	operator bool() const;
+	bool operator!() const;
+	String operator*() const;
+
+	friend bool operator==(const DirectoryIterator& lhs,
+		                   const DirectoryIterator& rhs);
+
+private:
+	static String pathToString(const Path& p);
+
+private:
+	void tryToStartIterationIn(const String& path);
+	void skipNonpassingFiles();
+	bool isValid() const;
+	String currentExtension() const;
+
+private:
+	static const Iterator end;
+
+private:
+	ExtensionFilter filter;
+	Iterator iterator;
+};
+
+bool operator!=(const DirectoryIterator& lhs, const DirectoryIterator& rhs);
+
+#endif //__DIRECTORY_ITERATOR_HEADER_INCLUDED__
