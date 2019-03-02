710cc32d2c042e6445dca43dd2fd036f59e456ed
StiliyanDr <stiliyan.dragnev@gmail.com>
2019-03-02 22:42:01 +0200

    Add some Iterator related convenience functions

--- Graph Store/Graph Store/Iterator/Iterator.h
+++ Graph Store/Graph Store/Iterator/Iterator.h
@@ -29,23 +29,44 @@ using Iterator = AbstractIterator<Item, false>;
 template <class Item>
 using ConstIterator = AbstractIterator<Item, true>;
 
-template <class Iterator, class Function>
-void forEach(Iterator& iterator, const Function& function)
+template <class Iterator, class UnaryFunction>
+void forEach(Iterator& iterator, UnaryFunction f)
 {
 	while (iterator)
 	{
-		function(*iterator);
+		f(*iterator);
 
 		++iterator;
 	}
 }
 
-template <class Iterator, class Predicate>
-Iterator skipUntil(Iterator& iterator, const Predicate& predicate)
+template <class Iterator, class UnaryPredicate>
+inline bool allOf(Iterator& iterator, UnaryPredicate p)
+{
+	return !anyOf(iterator, unaryComplementOf(p));
+}
+
+template <class UnaryPredicate>
+UnaryPredicate unaryComplementOf(UnaryPredicate p)
+{
+	return [p](auto&& x)
+	{
+		return !p(std::forward<decltype(x)>(x));
+	};
+}
+
+template <class Iterator, class UnaryPredicate>
+inline bool anyOf(Iterator& iterator, UnaryPredicate p)
+{
+	return static_cast<bool>(skipUntil(iterator, p));
+}
+
+template <class Iterator, class UnaryPredicate>
+Iterator skipUntil(Iterator& iterator, UnaryPredicate p)
 {
 	while (iterator)
 	{
-		if (predicate(*iterator))
+		if (p(*iterator))
 		{
 			break;
 		}
