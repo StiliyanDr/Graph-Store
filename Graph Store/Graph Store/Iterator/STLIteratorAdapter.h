8710d4d93731618e2af7f26cdbaafdf83104ce04
StiliyanDr <stiliyan.dragnev@gmail.com>
2019-03-09 21:45:20 +0200

    Modify the STLIteratorAdapter class

--- Graph Store/Graph Store/Iterator/STLIteratorAdapter.h
+++ Graph Store/Graph Store/Iterator/STLIteratorAdapter.h
@@ -2,72 +2,62 @@
 #define __STL_ITERATOR_ADAPTER_HEADER_INCLUDED__
 
 #include "Iterator.h"
-#include <stdexcept>
+#include <assert.h>
 
 template <class STLIterator, bool isConst = false>
 class STLIteratorAdapter : public AbstractIterator<typename STLIterator::value_type, isConst>
 {
 public:
-	STLIteratorAdapter(STLIterator rangeStart, STLIterator rangeEnd) :
-		iterator(rangeStart),
-		rangeEnd(rangeEnd)
-	{
-	}
+	STLIteratorAdapter(STLIterator rangeStart, STLIterator rangeEnd);
+	STLIteratorAdapter<STLIterator, isConst>& operator++() override;
 
-	virtual STLIteratorAdapter<STLIterator, isConst>& operator++() override
-	{
-		if (isValid())
-		{
-			++iterator;
-		}
-
-		return *this;
-	}
-
-	virtual Reference operator*() const override
-	{
-		return getCurrentItem();
-	}
-
-	virtual Pointer operator->() const override
-	{
-		return &getCurrentItem();
-	}
-	
-	virtual bool operator!() const noexcept override
-	{
-		return !isValid();
-	}
-	
-	virtual operator bool() const noexcept override
-	{
-		return isValid();
-	}
+protected:
+	bool isValid() const override;
 
 private:
-	Reference getCurrentItem() const
-	{
-		if (isValid())
-		{
-			return *iterator;
-		}
-		else
-		{
-			throw std::out_of_range("Iterator out of range!");
-		}
-	}
-
-	bool isValid() const noexcept
-	{
-		return iterator != rangeEnd;
-	}
+	Reference getCurrentItem() const override;
 
 private:
 	STLIterator iterator;
-	STLIterator rangeEnd;
+	STLIterator end;
 };
 
 template <class STLConstIterator>
 using STLConstIteratorAdapter = STLIteratorAdapter<STLConstIterator, true>;
+
+template <class STLIterator, bool isConst>
+STLIteratorAdapter<STLIterator, isConst>::STLIteratorAdapter(STLIterator rangeStart,
+	                                                         STLIterator rangeEnd) :
+	iterator(rangeStart),
+	end(rangeEnd)
+{
+}
+
+template <class STLIterator, bool isConst>
+STLIteratorAdapter<STLIterator, isConst>&
+STLIteratorAdapter<STLIterator, isConst>::operator++()
+{
+	if (isValid())
+	{
+		++iterator;
+	}
+
+	return *this;
+}
+
+template <class STLIterator, bool isConst>
+inline bool STLIteratorAdapter<STLIterator, isConst>::isValid() const
+{
+	return iterator != end;
+}
+
+template <class STLIterator, bool isConst>
+inline typename STLIteratorAdapter<STLIterator, isConst>::Reference
+STLIteratorAdapter<STLIterator, isConst>::getCurrentItem() const
+{
+	assert(isValid());
+
+	return *iterator;
+}
 
 #endif //__STL_ITERATOR_ADAPTER_HEADER_INCLUDED__
