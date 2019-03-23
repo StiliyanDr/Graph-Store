2ff2c4fd09174bde0584969c305ff74f7c2396c5
StiliyanDr <stiliyan.dragnev@gmail.com>
2019-03-23 23:18:18 +0200

    Change the main function of the program

--- Graph Store/Graph Store/Graph Store.cpp
+++ Graph Store/Graph Store/Graph Store.cpp
@@ -1,8 +1,16 @@
 #include "Application\Application.h"
+#include <iostream>
 
-int main()
-{
-	Application::instance().run();
+int main(int argc, const char* argv[])
+{	
+	if (argc != 2)
+	{
+		std::cerr << "Expected one argument: <directory>!\nUsage: "
+				  << argv[0] << '\n';
+		return 1;
+	}
+
+	Application::instance().runIn(argv[1]);
 
     return 0;
 }
