dbee52acc01d1a9d52d6afb5513a61dfbfe9e0e6
StiliyanDr <stiliyan.dragnev@gmail.com>
2019-03-28 23:24:09 +0200

    Refactor the UseGraphCommand class

--- Graph Store/Graph Store/Command/Use Graph Command/UseGraphCommand.cpp
+++ Graph Store/Graph Store/Command/Use Graph Command/UseGraphCommand.cpp
@@ -1,37 +1,21 @@
 #include "UseGraphCommand.h"
-#include "../Command Registrator/CommandRegistrator.h"
-#include "../Exceptions/Missing Argument Exception/MissingArgumentException.h"
+#include "Command/Command Registrator/CommandRegistrator.h"
 
-static CommandRegistrator<UseGraphCommand> registrator("use-graph", "Sets a specified graph as the used one");
-
-void UseGraphCommand::execute(args::Subparser& parser)
-{
-	parseArguments(parser);
-	useGraph(graphID);
-}
+static CommandRegistrator<UseGraphCommand> registrator("use-graph",
+	                                                   "Sets a specified graph as the used one");
 
 void UseGraphCommand::parseArguments(args::Subparser& parser)
 {
-	args::Positional<String, StringReader> id(parser, "graph id", "The identifier of the graph to use");
+	args::Positional<String, StringReader> id(parser,
+		                                      "graph id",
+		                                      "The id of the graph to use");
 	parser.Parse();
-	setGraphID(id);
+
+	graphID = getValueOf(id);
 }
 
-void UseGraphCommand::setGraphID(args::Positional<String, StringReader>& id)
-{
-	if (id.Matched())
-	{
-		graphID = args::get(id);
-	}
-	else
-	{
-		throw MissingArgumentException(id.Name());
-	}
-}
-
-void UseGraphCommand::useGraph(const String& id)
+void UseGraphCommand::doExecute()
 {
 	GraphCollection& graphs = getGraphs();
-	Graph& graph = graphs[id];
-	setUsedGraph(graph);
+	setUsedGraph(graphs[graphID]);
 }
