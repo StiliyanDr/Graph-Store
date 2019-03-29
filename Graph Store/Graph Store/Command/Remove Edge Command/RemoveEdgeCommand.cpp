539d9400d38ee66007fe177c39f83d9111edd832
StiliyanDr <stiliyan.dragnev@gmail.com>
2019-03-29 23:32:43 +0200

    Refactor the RemoveEdgeCommand class

--- Graph Store/Graph Store/Command/Remove Edge Command/RemoveEdgeCommand.cpp
+++ Graph Store/Graph Store/Command/Remove Edge Command/RemoveEdgeCommand.cpp
@@ -1,56 +1,28 @@
 #include "RemoveEdgeCommand.h"
-#include "../Command Registrator/CommandRegistrator.h"
-#include "../Exceptions/Missing Argument Exception/MissingArgumentException.h"
+#include "Command\Command Registrator\CommandRegistrator.h"
 
 static CommandRegistrator<RemoveEdgeCommand> registrator("remove-edge",
 														 "Removes the edge between two specified vertices");
 
-void RemoveEdgeCommand::execute(args::Subparser& parser)
-{
-	parseArguments(parser);
-	removeEdge(startVertexID, endVertexID);
-}
-
 void RemoveEdgeCommand::parseArguments(args::Subparser& parser)
 {
-	args::Positional<String, StringReader> startID(parser, "start vertex id", "The identifier of the start vertex");
-	args::Positional<String, StringReader> endID(parser, "end vertex id", "The identifier of the end vertex");
-
+	args::Positional<String, StringReader> startID(parser,
+		                                           "start vertex id",
+		                                           "The id of the start vertex");
+	args::Positional<String, StringReader> endID(parser,
+		                                         "end vertex id",
+		                                         "The id of the end vertex");
 	parser.Parse();
 
-	setStartVertexID(startID);
-	setEndVertexID(endID);
+	this->startID = getValueOf(startID);
+	this->endID = getValueOf(endID);
 }
 
-void RemoveEdgeCommand::setStartVertexID(args::Positional<String, StringReader>& id)
-{
-	if (id.Matched())
-	{
-		startVertexID = args::get(id);
-	}
-	else
-	{
-		throw MissingArgumentException(id.Name());
-	}
-}
-
-void RemoveEdgeCommand::setEndVertexID(args::Positional<String, StringReader>& id)
-{
-	if (id.Matched())
-	{
-		endVertexID = args::get(id);
-	}
-	else
-	{
-		throw MissingArgumentException(id.Name());
-	}
-}
-
-void RemoveEdgeCommand::removeEdge(const String& startVertexID, const String& endVertexID)
+void RemoveEdgeCommand::doExecute()
 {
 	Graph& usedGraph = getUsedGraph();
-	Graph::Vertex& start = usedGraph.getVertexWithID(startVertexID);
-	Graph::Vertex& end = usedGraph.getVertexWithID(endVertexID);
+	Graph::Vertex& start = usedGraph.getVertexWithID(startID);
+	Graph::Vertex& end = usedGraph.getVertexWithID(endID);
 
 	usedGraph.removeEdge(start, end);
 }
--- Graph Store/Graph Store/Command/Remove Edge Command/RemoveEdgeCommand.h
+++ Graph Store/Graph Store/Command/Remove Edge Command/RemoveEdgeCommand.h
@@ -1,32 +1,20 @@
 #ifndef __REMOVE_EDGE_COMMAND_HEADER_INCLUDED__
 #define __REMOVE_EDGE_COMMAND_HEADER_INCLUDED__
 
-#include "../Abstract class/Command.h"
-#include "../String Reader/StringReader.h"
+#include "Command\Graph Command\GraphCommand.h"
 
-class RemoveEdgeCommand : public Command
+class RemoveEdgeCommand : public GraphCommand
 {
 public:
 	RemoveEdgeCommand() = default;
-	RemoveEdgeCommand(const RemoveEdgeCommand&) = delete;
-	RemoveEdgeCommand& operator=(const RemoveEdgeCommand&) = delete;
-	RemoveEdgeCommand(RemoveEdgeCommand&&) = delete;
-	RemoveEdgeCommand& operator=(RemoveEdgeCommand&&) = delete;
-	virtual ~RemoveEdgeCommand() = default;
-
-	virtual void execute(args::Subparser& parser) override;
 
 private:
-	static void removeEdge(const String& startVertexID, const String& endVertexID);
+	void parseArguments(args::Subparser& parser) override;
+	void doExecute() override;
 
 private:
-	void parseArguments(args::Subparser& parser);
-	void setStartVertexID(args::Positional<String, StringReader>& id);
-	void setEndVertexID(args::Positional<String, StringReader>& id);
-
-private:
-	String startVertexID;
-	String endVertexID;
+	String startID;
+	String endID;
 };
 
 #endif //__REMOVE_EDGE_COMMAND_HEADER_INCLUDED__
