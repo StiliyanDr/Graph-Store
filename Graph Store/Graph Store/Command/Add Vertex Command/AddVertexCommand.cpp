010e603cc41f934e03786550aa826338d26d200a
StiliyanDr <stiliyan.dragnev@gmail.com>
2019-04-07 22:53:33 +0300

    Refactor the AddVertexCommand class

--- Graph Store/Graph Store/Command/Add Vertex Command/AddVertexCommand.cpp
+++ Graph Store/Graph Store/Command/Add Vertex Command/AddVertexCommand.cpp
@@ -1,37 +1,21 @@
 #include "AddVertexCommand.h"
-#include "../Command Registrator/CommandRegistrator.h"
-#include "../Exceptions/Missing Argument Exception/MissingArgumentException.h"
+#include "Command\Command Registrator\CommandRegistrator.h"
 
-static CommandRegistrator<AddVertexCommand> registrator("add-vertex", "Adds a vertex with a specified identifier");
-
-void AddVertexCommand::execute(args::Subparser& parser)
-{
-	parseArguments(parser);
-	addVertex(vertexID);
-}
+static CommandRegistrator<AddVertexCommand> registrator("add-vertex",
+	                                                    "Adds a vertex with a specified id");
 
 void AddVertexCommand::parseArguments(args::Subparser& parser)
 {
-	args::Positional<String, StringReader> id(parser, "vertex id", "The identifier of the new vertex");
+	args::Positional<String, StringReader> id(parser,
+		                                      "vertex id",
+		                                      "The id of the new vertex");
 	parser.Parse();
-	setVertexID(id);
+	vertexID = getValueOf(id);
 }
 
-void AddVertexCommand::setVertexID(args::Positional<String, StringReader>& id)
-{
-	if (id.Matched())
-	{
-		vertexID = args::get(id);
-	}
-	else
-	{
-		throw MissingArgumentException(id.Name());
-	}
-}
-
-void AddVertexCommand::addVertex(const String& id)
+void AddVertexCommand::doExecute()
 {
 	Graph& usedGraph = getUsedGraph();
 	
-	usedGraph.addVertex(id);
+	usedGraph.addVertex(vertexID);
 }
--- Graph Store/Graph Store/Command/Add Vertex Command/AddVertexCommand.h
+++ Graph Store/Graph Store/Command/Add Vertex Command/AddVertexCommand.h
@@ -1,27 +1,16 @@
 #ifndef __ADD_VERTEX_COMMAND_HEADER_INCLUDED__
 #define __ADD_VERTEX_COMMAND_HEADER_INCLUDED__
 
-#include "../Abstract class/Command.h"
-#include "../String Reader/StringReader.h"
+#include "Command\Graph Command\GraphCommand.h"
 
-class AddVertexCommand : public Command
+class AddVertexCommand : public GraphCommand
 {
 public:
 	AddVertexCommand() = default;
-	AddVertexCommand(const AddVertexCommand&) = delete;
-	AddVertexCommand& operator=(const AddVertexCommand&) = delete;
-	AddVertexCommand(AddVertexCommand&&) = delete;
-	AddVertexCommand& operator=(AddVertexCommand&&) = delete;
-	virtual ~AddVertexCommand() = default;
-
-	virtual void execute(args::Subparser& parser) override;
 
 private:
-	static void addVertex(const String& id);
-
-private:
-	void parseArguments(args::Subparser& parser);
-	void setVertexID(args::Positional<String, StringReader>& id);
+	void parseArguments(args::Subparser& parser) override;
+	void doExecute() override;
 
 private:
 	String vertexID;
